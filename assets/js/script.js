18b0e2703a0f1d7239831bb2f298c7d222fc28ff
Mag Millen-Dutka <145022728+MagMillen-Dutka@users.noreply.github.com>
2023-11-01 18:18:20 +0000

    re-work of JS function for past-pres-future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,115 @@
-
   // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
   // the code isn't run until the browser has finished rendering all the elements
   // in the html.
+  var localeSettings = {};
+  dayjs.locale(localeSettings);
 
-  var localSettings = {};
-  dayjs.locale(localSettings);
+  // Date and time at the top of the scheduler
+  function dateAndTime() {
+    var dateElement = $('#date');
+    var timeElement = $('#time');
+    var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+    var currentTime = dayjs().format('HH:mm:ss');
+    dateElement.text(currentDate);
+    timeElement.text(currentTime);
+  }
+  setInterval(dateAndTime, 1000);
 
-  // Wait until the DOM is fully loaded before executing the code inside the function.
-  $(function () {
+  // main function coding for scheduler - including local stored info, time-block colour changes and save button working.
 
-    // Get the current hour of the day using the dayjs library.
-    const currentHour = dayjs().format('H');
+$(function () {
+    
+    var currentHour = dayjs().format('H');
 
-  // The function below changes the color of each time block based on whether it's in the "past, present, or future" relative to the current hour.
-    function timeBlocks() {
-      $('.time-block').each(function() {
-        var blockHour = parseInt(this.id);
-        $(this).toggleClass('past', blockHour < currentHour);
-        $(this).toggleClass('present', blockHour === currentHour);
-        $(this).toggleClass('future', blockHour > currentHour);
+
+    function colorBlock() {  
+      $('.time-block').each(function() {  
+        var blockHour = $(this).attr("id").split("hour-")[1]; 
+        console.log("currentHour" + currentHour)
+        console.log("blockHour" + blockHour)
+        if (blockHour == currentHour) {
+          $(this).removeClass("past");
+          $(this).removeClass("future");
+          $(this).addClass("present");
+         } else if (blockHour < currentHour) {
+          $(this).removeClass("present");
+          $(this).removeClass("future");
+          $(this).addClass("past");
+        } else if (blockHour > currentHour) {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        }
+        
+
       });
-    }
-
+    // }
   // The  function below will save the user's input in a textarea to localStorage - only when the corresponding save button has been clicked.
-    function textEntry() {
+    // function textEntry() {
       $('.saveBtn').on('click', function() {
-        const key = $(this).parent().attr('id');
-        const value = $(this).siblings('.description').val();
+        var key = $(this).parent().attr('id');
+        var value = $(this).siblings('.description').val();
         localStorage.setItem(key, value);
       });
-    }
+    // }
 
-   // The function below will refresh the color of each time block based on whether it's in the past(grey), present(red), or future(green) relative to the current time. 
-    function refreshColor() {
       $('.time-block').each(function() {
-        const blockHour = parseInt(this.id);
-        if (blockHour == currentHour) {
-          $(this).removeClass('past future').addClass('present');
-        } else if (blockHour < currentHour) {
-          $(this).removeClass('future present').addClass('past');
-        } else {
-          $(this).removeClass('past present').addClass('future');
-        }
+        var key = $(this).attr('id');
+        var value = localStorage.getItem(key);
+        $(this).children('.description').val(value);
       });
     }
+   // The function below will refresh the color of each time block based on whether it's in the past(grey), present(red), or future(green) relative to the current time. 
+    // function colorBlock() {
+    //   $('.time-block').each(function() {
+    //     const blockHour = parseInt(this.id);
+    //     if (blockHour == currentHour) {
+    //       $(this).removeClass('past future').addClass('present');
+    //     } else if (blockHour < currentHour) {
+    //       $(this).removeClass('future present').addClass('past');
+    //     } else {
+    //       $(this).removeClass('past present').addClass('future');
+    //     }
+    //   });
 
-    // This will get the user input from the localStorage and set textarea values for each time block.
-    $('.time-block').each(function() {
-      const key = $(this).attr('id');
-      const value = localStorage.getItem(key);
-      $(this).children('.description').val(value);
-    });
-  
-    // Please note: this is my favourtie part of the module - I absolutly love the display of current date and time especially 
-    // since the the time referesed every second - you can find this among the header of the page!
-    function updateTime() {
-      const dateElement = $('#date');
-      const timeElement = $('#time');
-      const currentDate = dayjs().format('dddd, MMMM D, YYYY');
-      const currentTime = dayjs().format('hh:mm:ss A');
-      dateElement.text(currentDate);
-      timeElement.text(currentTime);
-    }
+
+   
     // Call the three main functions to set up the page.
-    timeBlocks();
-    textEntry();                
-    refreshColor();
+    // hourlyColor();
+    // textEntry();                
+    colorBlock();
+   
+});
 
-    // This will update the time once per second for the current time once per second using setInterval() 
-    // giving my beautiful diplay header a live time hours, minues and seconds coutner
-    setInterval(updateTime, 1000);
-  });
+
+
+  // $(".block").each(function () {
+
+  //   // variable "hour" holds id hour from class block and pareInt is used to chang it from a string to an integer.
+  //   var hour = parseInt($(this).attr("id"));
+
+  //   // if statement to ditermin if in the past hour's
+  //   if (hour < currentHour) {
+  //       // adds grey to blocks
+  //       $(this).addClass("past");
+  //   }
+
+
+  //     //else if statement to ditermin if in the present hour
+  //     else if (hour === currentHour) {
+  //       // removes grey to blocks
+  //       $(this).removeClass("past");
+  //       // adds red to blocks
+  //       $(this).addClass("present");
+  //   }
+
+  //   // else statement to ditermin if in the future's
+  //   else {
+  //       // removes grey to blocks
+  //       $(this).removeClass("past");
+  //       // removes red to blocks
+  //       $(this).removeClass("present");
+  //       // adds green to blocks
+  //       $(this).addClass("future");
+  //   }
+  // });
