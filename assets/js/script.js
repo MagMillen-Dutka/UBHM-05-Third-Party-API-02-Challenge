d647efe7f5d4b5f011814a50695818ba43d8ece4
Mag Millen-Dutka <145022728+MagMillen-Dutka@users.noreply.github.com>
2023-11-02 11:08:28 +0000

    update of comments on JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-  // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-  // the code isn't run until the browser has finished rendering all the elements
-  // in the html.
+// Global variables and local window commands for time.
   var localeSettings = {};
   dayjs.locale(localeSettings);
 
-  // Date and time at the top of the scheduler
+// Date and time at the top of the scheduler in the header section. 
+// Capital HH on currentTime switches time from 12 hour to 24 hour clock.
+// Interval is also set here for refreshing the date and time and this is set to  refreshing every 1 second (1000).
   function dateAndTime() {
     var dateElement = $('#date');
     var timeElement = $('#time');
@@ -15,12 +15,11 @@
   }
   setInterval(dateAndTime, 1000);
 
-  // main function coding for scheduler - including local stored info, time-block colour changes and save button working.
+// main function coding for scheduler - including local stored info, time-block colour changes and save button.
 
 $(function () {
     
     var currentHour = dayjs().format('H');
-
 
     function colorBlock() {  
       $('.time-block').each(function() {  
@@ -40,76 +39,23 @@ $(function () {
           $(this).removeClass("present");
           $(this).addClass("future");
         }
-        
-
       });
-    // }
-  // The  function below will save the user's input in a textarea to localStorage - only when the corresponding save button has been clicked.
-    // function textEntry() {
+  
+// The  function below will save the user's input in a textarea to localStorage - only when the corresponding save button has been clicked.
       $('.saveBtn').on('click', function() {
         var key = $(this).parent().attr('id');
         var value = $(this).siblings('.description').val();
         localStorage.setItem(key, value);
       });
-    // }
-
+    
       $('.time-block').each(function() {
         var key = $(this).attr('id');
         var value = localStorage.getItem(key);
         $(this).children('.description').val(value);
       });
     }
-   // The function below will refresh the color of each time block based on whether it's in the past(grey), present(red), or future(green) relative to the current time. 
-    // function colorBlock() {
-    //   $('.time-block').each(function() {
-    //     const blockHour = parseInt(this.id);
-    //     if (blockHour == currentHour) {
-    //       $(this).removeClass('past future').addClass('present');
-    //     } else if (blockHour < currentHour) {
-    //       $(this).removeClass('future present').addClass('past');
-    //     } else {
-    //       $(this).removeClass('past present').addClass('future');
-    //     }
-    //   });
-
-
    
-    // Call the three main functions to set up the page.
-    // hourlyColor();
-    // textEntry();                
+// calling on the function above for colour changing.              
     colorBlock();
    
 });
-
-
-
-  // $(".block").each(function () {
-
-  //   // variable "hour" holds id hour from class block and pareInt is used to chang it from a string to an integer.
-  //   var hour = parseInt($(this).attr("id"));
-
-  //   // if statement to ditermin if in the past hour's
-  //   if (hour < currentHour) {
-  //       // adds grey to blocks
-  //       $(this).addClass("past");
-  //   }
-
-
-  //     //else if statement to ditermin if in the present hour
-  //     else if (hour === currentHour) {
-  //       // removes grey to blocks
-  //       $(this).removeClass("past");
-  //       // adds red to blocks
-  //       $(this).addClass("present");
-  //   }
-
-  //   // else statement to ditermin if in the future's
-  //   else {
-  //       // removes grey to blocks
-  //       $(this).removeClass("past");
-  //       // removes red to blocks
-  //       $(this).removeClass("present");
-  //       // adds green to blocks
-  //       $(this).addClass("future");
-  //   }
-  // });
